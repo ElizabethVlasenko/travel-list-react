faabc9bb702cf94f50faed592f2f697c228a78e3
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-03-07 15:32:54 +0000

    Created the base layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 export default function App() {
   return (
-    <>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
       <Stats />
-    </>
+    </div>
   );
 }
 
-function Logo() {}
+function Logo() {
+  return <h1>🌴 Far away 🍸</h1>;
+}
 
-function Form() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
 
-function PackingList() {}
+function PackingList() {
+  return <div className="list">List</div>;
+}
 
-function Stats() {}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list and you already packed X% 🛫 </em>
+    </footer>
+  );
+}
